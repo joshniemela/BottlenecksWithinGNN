4cb18ac3932d57a3b47dd8ef7e48ef0f605793d4
Mustafa Hekmat <mustafahekmat@hotmail.dk>
2024-09-09 08:25:02 +0200

    added GPU support

--- tree_neighbours_match/dataset.py
+++ tree_neighbours_match/dataset.py
@@ -5,7 +5,7 @@ from torch_geometric.data import Data
 from torch_geometric.utils import add_self_loops
 
 
-def generate_tree(n: int, depth: int) -> list[Data]:
+def generate_tree(n: int, depth: int, device="cpu") -> list[Data]:
     """
     Generate n binary trees with specified depth and number of trees.
 
@@ -22,23 +22,30 @@ def generate_tree(n: int, depth: int) -> list[Data]:
     num_last_layer = 2**depth
     halfway_index = num_nodes // 2
 
-    node_values = torch.empty(n, num_nodes, 2, dtype=torch.int)
+    node_values = torch.empty(n, num_nodes, 2, dtype=torch.int, device=device)
 
     # Set the first half in each tree to 0s, this is the part of the tree with no leaves
     node_values[:, :halfway_index, :] = 0
 
     # Set the second half first slice to the values from 1 to num_last_layer + 1.
     # (We are enumerating the nodes on the last layer)
-    node_values[:, halfway_index:, 0] = torch.arange(1, num_last_layer + 1)
+    node_values[:, halfway_index:, 0] = torch.arange(
+        1, num_last_layer + 1, device=device
+    )
     # Set the second half second slice to a random permutation from 1 to n
     # (We are setting the number of neighbours of the nodes on the last layer)
     node_values[:, halfway_index:, 1] = (
-        torch.randint(torch.iinfo(torch.int64).max, (n, num_last_layer)).argsort() + 1
+        torch.randint(
+            torch.iinfo(torch.int64).max, (n, num_last_layer), device=device
+        ).argsort()  # Random permutation
+        + 1
     )
 
     # Pick a random row from the second of the now random tree
-    random_row_indexes = torch.randint(halfway_index, num_nodes, (n, 1)).squeeze()
-    random_rows = node_values[torch.arange(n), random_row_indexes]
+    random_row_indexes = torch.randint(
+        halfway_index, num_nodes, (n, 1), device=device
+    ).squeeze()
+    random_rows = node_values[torch.arange(n, device=device), random_row_indexes]
 
     # We now set the y value to the class of this row
     # and we set the number of neighbours of the root node to the neighours of the row
@@ -46,8 +53,8 @@ def generate_tree(n: int, depth: int) -> list[Data]:
     node_values[:, 0, 1] = random_rows[:, 1]
 
     # Create the edge list (it is a tree so we know it is n-1 edges)
-    edge_index = torch.empty((2, num_nodes - 1), dtype=torch.int)
-    edge_index[0, :] = torch.arange(1, num_nodes)
+    edge_index = torch.empty((2, num_nodes - 1), dtype=torch.int, device=device)
+    edge_index[0, :] = torch.arange(1, num_nodes, device=device)
     edge_index[1, :] = (edge_index[0, :] - 1) // 2
     edge_index = add_self_loops(edge_index, num_nodes=num_nodes)[0]
     edge_index = edge_index.repeat(n, 1, 1)
@@ -63,5 +70,6 @@ def generate_tree(n: int, depth: int) -> list[Data]:
 
 if __name__ == "__main__":
     # Generate 2 binary trees with depth 3
-    data = generate_tree(2, 3)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    data = generate_tree(10000, 6, device)
     print(data[0].x, data[0].y, data[0].edge_index)
