0fc94cacd606ba23d36dcb92d84da9d89fa080a7
Mustafa H. <mustafahekmat@hotmail.dk>
2024-09-10 15:34:33 +0200

    cleanup

--- tree_neighbours_match/train.py
+++ tree_neighbours_match/train.py
@@ -1,5 +1,5 @@
 import torch
-from torch.nn import functional as F
+import torch.nn as nn
 from torch.optim import Adam
 from torch import cuda
 from torch_geometric.loader import DataLoader
@@ -7,90 +7,91 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 import dataset as ds
 from models import GCN
 
-epochs = 1000
-accum_grad = 1
-eval_every = 1000
-patience = 20
+
+# This should be set to an arbitrary high but not infinite value
+max_epochs = 1000
+
+early_stop_patience = 20
 
 device = "cuda" if cuda.is_available() else "cpu"
+
+# Debugger is more happy if we use the CPU
 device = "cpu"
 
 model = GCN(9, 32, 9, 4).to(device)
-criterion = F.cross_entropy
-optimiser = Adam(model.parameters(), lr=0.001)
+criterion = nn.CrossEntropyLoss(reduction="sum")
+optimiser = Adam(model.parameters(), lr=0.02)
 scheduler = ReduceLROnPlateau(
-    optimiser, mode="max", threshold_mode="abs", factor=0.5, patience=10
+    optimiser, mode="max", threshold_mode="abs", factor=0.75, patience=5
 )
 print("Starting training")
 
 train_data, test_data = ds.train_test_split(ds.generate_tree(10000, 3, device))
-train_loader = DataLoader(train_data, batch_size=516, pin_memory=True)
-test_loader = DataLoader(test_data, batch_size=32, pin_memory=True)
+train_loader = DataLoader(train_data, batch_size=2048, pin_memory=True)
+eval_loader = DataLoader(test_data, batch_size=128, pin_memory=True)
 
+total_train_samples = len(train_loader.dataset)
+total_eval_samples = len(eval_loader.dataset)
 best_test_acc = 0.0
 best_train_acc = 0.0
 best_epoch = 0
 epochs_no_improve = 0
 
 
-for epoch in range(epochs):
+for epoch in range(max_epochs):
+    # Training step
     model.train()
     total_loss = 0
-    total_num_examples = 0
     train_correct = 0
     optimiser.zero_grad()
-    for i, batch in enumerate(train_loader):
+    for batch in train_loader:
         batch = batch.to(device)
-        out = model(batch)
-        loss = criterion(out, batch.y)
-        total_num_examples += batch.num_graphs
-        total_loss += loss.item() * batch.num_graphs
-        _, train_pred = out.max(dim=1)
+        train_preds = model(batch)
+        loss = criterion(train_preds, batch.y)
+        total_loss += loss.item()
+        train_pred = train_preds.argmax(dim=1)
         train_correct += train_pred.eq(batch.y).sum().item()
 
-        loss = loss / accum_grad
         loss.backward()
-        if (i + 1) % accum_grad == 0:
-            optimiser.step()
-            optimiser.zero_grad()
 
-    avg_training_loss = total_loss / total_num_examples
-    train_acc = train_correct / total_num_examples
+        optimiser.step()
+        optimiser.zero_grad()
+
+    avg_training_loss = total_loss / total_train_samples
+    train_acc = train_correct / total_train_samples
     scheduler.step(train_acc)
 
+    # Evaluation step
     model.eval()
     with torch.no_grad():
-
-        total_correct = 0
-        total_examples = 0
-        for batch in test_loader:
+        total_test_correct = 0
+        for batch in eval_loader:
             batch = batch.to(device)
-            _, pred = model(batch).max(dim=1)
-            total_correct += pred.eq(batch.y).sum().item()
-            total_examples += batch.y.size(0)
-        test_acc = total_correct / total_examples
+            eval_preds = model(batch).argmax(dim=1)
+            total_test_correct += eval_preds.eq(batch.y).sum().item()
+        eval_acc = total_test_correct / total_eval_samples
 
     cur_lr = [g["lr"] for g in optimiser.param_groups]
 
     new_best_str = ""
     stopping_threshold = 0.0001
-    stopping_value = 0
 
     if train_acc > best_train_acc + stopping_threshold:
         best_train_acc = train_acc
-        best_test_acc = test_acc
+        best_test_acc = eval_acc
         best_epoch = epoch
         epochs_no_improve = 0
-        stopping_value = train_acc
-        new_best_str = " (new best train)"
+        new_best_train = True
     else:
         epochs_no_improve += 1
+        new_best_train = False
+
     print(
-        f"Epoch {epoch}, LR: {cur_lr}: Train loss: {avg_training_loss:.7f}, Train acc: {train_acc:.4f}, Test accuracy: {test_acc:.4f}{new_best_str}"
+        f"Epoch {epoch}, LR: {cur_lr}: Train loss: {avg_training_loss:.7f}, Train acc: {train_acc:.4f}, Test accuracy: {eval_acc:.4f}",
+        "(new best train)" if new_best_train else "",
     )
-    if stopping_value == 1.0:
-        break
-    if epochs_no_improve >= patience:
-        print(f"{patience} epochs without train acc improvement, stopping. ")
+
+    if epochs_no_improve >= early_stop_patience or train_acc == 1.0:
+        print(f"{early_stop_patience} epochs without train acc improvement, stopping. ")
         break
 print(f"Best train acc: {best_train_acc}, epoch: {best_epoch}")
