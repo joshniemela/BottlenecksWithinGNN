197d5142e9233d7a5f33aa88b352e745ff1b360d
Joshua Niemelä <josh@jniemela.dk>
2024-09-11 18:40:34 +0200

    Made model work on GPU and added fully adjacent

--- tree_neighbours_match/models.py
+++ tree_neighbours_match/models.py
@@ -5,9 +5,26 @@ import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
 
 
+def create_fully_adjacent_edges(num_nodes, num_trees):
+    """
+    Creates an edge list which connects every single node to the root for each tree
+    Args:
+        num_nodes (int): The number of nodes in each binary tree.
+        num_trees (int): The number of binary trees (batch size).
+    Returns:
+        torch.Tensor: A tensor of shape (2, num_nodes * num_trees) representing the edge list.
+    """
+    # We need to connect all nodes to a root, therefore we can just count from 1 to num_nodes for each tree
+    source_nodes = torch.arange(num_nodes * num_trees)
+    # Floor division to get the root node for each tree
+    target_nodes = source_nodes // num_nodes
+    return torch.stack([source_nodes, target_nodes])
+
 class GCN(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim, num_layers):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers, use_fully_adj=False):
         super(GCN, self).__init__()
+        self.use_fully_adj = use_fully_adj
+        self.num_layers = num_layers
 
         self.classes_embedding = nn.Embedding(
             num_embeddings=input_dim, embedding_dim=hidden_dim
@@ -29,10 +46,19 @@ class GCN(nn.Module):
     def forward(self, data):
         x, edge_index, batch = data.x, data.edge_index, data.batch
 
+        # Total number of nodes in a single tree
         num_nodes = (x.shape[0] / (data.batch[-1] + 1)).int().item()
+        num_batches = batch[-1] + 1
+
+        # Use fully adjacent edges if specified
+        edge_index = create_fully_adjacent_edges(num_nodes, num_batches).to(x.device) if self.use_fully_adj else edge_index
+
+        # We set the roots mask to be 1, 0,..., 1, 0...
+        # where 1 is the root and 0 is not a root.
+        # this is needed because batching means we have multiple graphs in one batch
         root_mask = torch.zeros(num_nodes, dtype=torch.bool)
         root_mask[0] = True
-        roots_mask = root_mask.repeat(batch[-1] + 1)
+        roots_mask = root_mask.repeat(num_batches)
 
         x_classes, x_neighbours = x[:, 0], x[:, 1]
         x_classes = self.classes_embedding(x_classes)
@@ -49,3 +75,4 @@ class GCN(nn.Module):
         logits = self.output_layer(root_nodes)
 
         return logits
+
