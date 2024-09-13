d522d19cfc85c7f4de4c6f0c423d91de17afb8eb
Joshua Niemelä <josh@jniemela.dk>
2024-09-13 09:02:31 +0200

    Change sweep config

--- tree_neighbours_match/train.py
+++ tree_neighbours_match/train.py
@@ -194,7 +194,36 @@ def create_dataset_train_eval_model():
     # Run general training function
     train_eval_model(model, train_loader, eval_loader, training_config)
 
+    # Finish wandb run
+    wandb.finish()
+
+
+model_dict = {"GAT": GAT, "GCN": GCN, "GGNN": GGNN, "GIN": GIN}
+
+# Sweep Configuration for hyperparameter search
+sweep_config = {
+    "method": "grid",
+    "metric": {"name": "train_accuracy", "goal": "maximize"},
+    "parameters": {
+        "model_type": {"values": ["GCN", "GGNN", "GIN", "GAT"]},
+        "tree_depth": {"values": [2, 3, 4]},
+        "num_trees": {"values": [40000]},
+        "epochs": {"values": [1000]},
+        # Initial learning rate (intentionally too high)
+        "lr": {"values": [0.01]},
+        # FIXME: setting this to 4096 causes a bizarre error
+        "batch_size": {"values": [2048]},
+        "hidden_dim": {"values": [32]},
+        "early_stop_patience": {"values": [20]},
+        "early_stop_grace_period": {"values": [50]},
+        "stopping_threshold": {"values": [0.0001]},
+        # If scheduler runs out of patience, it multiplies current LR by this factor
+        "scheduler_factor": {"values": [0.75]},
+        # Number of epochs to wait before reducing LR
+        "scheduler_patience": {"values": [5]},
+        "fully_adjacent_last_layer": {"values": [False]},
+    },
+}
 
 if __name__ == "__main__":
-
     create_dataset_train_eval_model()
