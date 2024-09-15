4f1dbcb86833bec2748c11247a87cf77ec6dd458
Joshua Niemelä <josh@jniemela.dk>
2024-09-15 18:17:50 +0200

    Add yaml, change train and update .gitignore

--- tree_neighbours_match/train.py
+++ tree_neighbours_match/train.py
@@ -177,34 +177,6 @@ def train_eval_gcn_with_wandb():
 
 model_dict = {"GAT": GAT, "GCN": GCN, "GGNN": GGNN, "GIN": GIN}
 
-# Sweep Configuration for hyperparameter search
-sweep_config = {
-    "method": "bayes",
-    "metric": {"name": "train_accuracy", "goal": "maximize"},
-    "parameters": {
-        "model_type": {"values": ["GCN"]},
-        "tree_depth": {"values": [3]},
-        "num_trees": {"values": [40000]},
-        "epochs": {"values": [1000]},
-        # Initial learning rate (intentionally too high)
-        "lr": {"values": [0.01]},
-        # FIXME: setting this to 4096 causes a bizarre error
-        "batch_size": {"values": [2048]},
-        "hidden_dim": {"values": [32]},
-        "early_stop_patience": {"values": [20]},
-        "early_stop_grace_period": {"values": [50]},
-        "stopping_threshold": {"values": [0.0001]},
-        # If scheduler runs out of patience, it multiplies current LR by this factor
-        "scheduler_factor": {"values": [0.75]},
-        # Number of epochs to wait before reducing LR
-        "scheduler_patience": {"values": [5]},
-        "fully_adjacent_last_layer": {"values": [False]},
-    },
-}
-
 if __name__ == "__main__":
-    # Initialize sweep
-    sweep_id = wandb.sweep(sweep_config, project="TreeBottleneckReproduction")
-
     # Run the sweep
-    wandb.agent(sweep_id, function=train_eval_gcn_with_wandb, count=1)
+    train_eval_gcn_with_wandb()
