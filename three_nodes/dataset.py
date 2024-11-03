8f6382a2312e942366b477e57a7502f98785c426
Mustafa H. <mustafahekmat@hotmail.dk>
2024-11-03 17:16:57 +0100

    three nodes reimplemented

--- three_nodes/dataset.py
+++ three_nodes/dataset.py
@@ -1,7 +1,8 @@
 import torch
 from torch_geometric.data import Data
 
-def generate_regression_problem(n: int, device="cpu") -> list[Data]:
+
+def generate_three_nodes_dataset(n: int, device="cpu") -> list[Data]:
     # Index 0 is our root, 1 and 2 are our two nodes on the sides
     edge_index = torch.tensor([[1, 2], [0, 0]])
 
@@ -13,13 +14,15 @@ def generate_regression_problem(n: int, device="cpu") -> list[Data]:
     # Set the two side nodes to something between 0 and 100
     node_values[:, 1:] = torch.randint(100, (n, 2))
 
+    node_values = node_values.reshape(n, 3, 1).float()
+
     # Find the sum of each graph, we skip the root, this is technically unnessecary since its set to 0
     y = torch.sum(node_values[:, 1:], 1)
 
     # We may use the data_list directly in a DataLoader object,
     # https://pytorch-geometric.readthedocs.io/en/latest/notes/create_dataset.html
     data_list = [
-        Data(x=node_values[i], y=y[i], edge_index=edge_index[i]) for i in range(n)
+        Data(x=node_values[i], y=y[i], edge_index=edge_index) for i in range(n)
     ]
 
     return data_list
