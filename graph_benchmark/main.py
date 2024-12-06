301b81d5048f1614fc4d72599cb04034c997e8f9
Joshua Niemelä <josh@jniemela.dk>
2024-12-06 11:22:25 +0100

    Added sweep for graph benchmark

--- graph_benchmark/main.py
+++ graph_benchmark/main.py
@@ -8,6 +8,8 @@ from fully_adjacent import fully_connect
 from torch_geometric.nn.aggr import SoftmaxAggregation, SetTransformerAggregation
 from torch import nn
 from new_fully_adjacent import GlobalSAGEConv
+import csv
+from pathlib import Path
 
 
 class GCN(nn.Module):
@@ -33,26 +35,20 @@ class GCN(nn.Module):
         for _ in range(num_hidden_layers):
             self.layer_norms.append(nn.LayerNorm(hidden_dim))
 
-        self.output_layer = GlobalSAGEConv(hidden_dim, output_dim, bias=True)
-        # self.output_layer = GCNConv(hidden_dim, output_dim, bias=True)
+        if self.use_fully_adj:
+            self.output_layer = GlobalSAGEConv(hidden_dim, output_dim, bias=True)
+        else:
+            self.output_layer = GCNConv(hidden_dim, output_dim, bias=True)
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
-        if self.use_fully_adj:
-            if data.full_edge_index is None:
-                raise ValueError(
-                    "full_edge_index is None, please run fully_connect on the dataset"
-                )
-            full_edge_index = data.full_edge_index
 
         x = self.input_layer(x, edge_index)
         for i, layer in enumerate(self.layers):
             x = layer(x, edge_index)
             x = F.relu(x)
 
-        return self.output_layer(
-            x, full_edge_index if self.use_fully_adj else edge_index
-        )
+        return self.output_layer(x, edge_index)
 
 
 def train(model, data, optimizer):
@@ -74,11 +70,56 @@ def test(model, data):
     return test_acc
 
 
-def main():
+def add_run_to_csv(dataset_name, use_fully_adj, num_hidden_layers, learning_rate):
+    csv_file_path = f"results/{dataset_name}.csv"
+    csv_file = Path(csv_file_path)
+
+    if not csv_file.exists():
+        with open(csv_file, "w") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(
+                [
+                    "Dataset",
+                    "Use Fully Adj",
+                    "Num Hidden Layers",
+                    "Learning Rate",
+                ]
+            )
+
+    with open(csv_file, "a") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(
+            [
+                dataset_name,
+                use_fully_adj,
+                num_hidden_layers,
+                learning_rate,
+            ]
+        )
+
+
+def sweep_all():
+    for dataset_name in ["Cora", "CiteSeer", "PubMed"]:
+        for use_fully_adj in [True, False]:
+            for num_hidden_layers in [1, 2, 3]:
+                for learning_rate in [0.001, 0.002, 0.005, 0.001]:
+                    for i in range(15):
+                        sweep(
+                            dataset_name,
+                            use_fully_adj,
+                            num_hidden_layers,
+                            learning_rate,
+                        )
+
+
+def sweep(dataset_name, use_fully_adj, num_hidden_layers, learning_rate):
+    print(
+        f"Sweeping {dataset_name} with FA:{use_fully_adj} and {num_hidden_layers} hidden layers and learning rate {learning_rate}"
+    )
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Load dataset (Cora by default)
-    dataset = CitationDataset("CiteSeer")
+    dataset = CitationDataset(dataset_name)
 
     data = dataset.get_data().to(device)
 
@@ -87,22 +128,34 @@ def main():
         input_dim=dataset.num_features,
         hidden_dim=128,
         output_dim=dataset.num_classes,
-        num_hidden_layers=2,
-        use_fully_adj=False,
+        num_hidden_layers=num_hidden_layers,
+        use_fully_adj=use_fully_adj,
     ).to(device)
 
-    # Setup training
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.002, weight_decay=5e-4)
+    optimizer = torch.optim.Adam(
+        model.parameters(), lr=learning_rate, weight_decay=5e-4
+    )
 
+    best_acc = 0
+    epochs_no_improvement = 0
     # Training loop
-    for epoch in range(1000):
+    for epoch in range(2000):
         loss = train(model, data, optimizer)
-        if epoch % 20 == 0:
-            test_acc = test(model, data)
-            print(f"Epoch: {epoch:04d}, Loss: {loss:.4f}, Test Acc: {test_acc:.4f}")
+        test_acc = test(model, data)
+        if test_acc > best_acc:
+            best_acc = test_acc
+            epochs_no_improvement = 0
+            # print(f"Epoch: {epoch:04d}, Loss: {loss:.4f}, Test Acc: {test_acc:.4f}")
+        else:
+            epochs_no_improvement += 1
+        if epochs_no_improvement == 300:
+            break
 
-    print("Done!")
+    print(
+        f"Test Acc: {best_acc:.4f}, Params: {dataset_name}, {use_fully_adj}, {num_hidden_layers}, {learning_rate}"
+    )
+    add_run_to_csv(dataset_name, use_fully_adj, num_hidden_layers, learning_rate)
 
 
 if __name__ == "__main__":
-    main()
+    sweep_all()
