3042ebc0e0c24c26ffccc348eeafc0e7abbf8bcf
Mustafa H. <mustafahekmat@hotmail.dk>
2025-01-03 11:13:25 +0100

    Plotting score of tree-neighbours

--- tree_neighbours_match/summary.jl
+++ tree_neighbours_match/summary.jl
@@ -111,3 +111,51 @@ savefig(line_plot, "results/tree_neighbor_accuracy.svg")
 
 qq_plot = plot_score_qq_by_hidden_layers(results)
 savefig(qq_plot, "results/tree_neighbor_qq.svg")
+
+data = CSV.read("data.csv", DataFrame)
+
+function plot_score_distribution(df::DataFrame, tree_depth, λ=0.01)
+    filtered_df_no_fa = filter(row -> row.tree_depth == tree_depth && row.use_fully_adj == false, df)
+    filtered_df_fa = filter(row -> row.tree_depth == tree_depth && row.use_fully_adj == true, df)
+
+     # Check if there is data for either filtered DataFrame
+     if nrow(filtered_df_no_fa) == 0 && nrow(filtered_df_fa) == 0
+        return plot(title="No data for tree_depth=$tree_depth")
+    end
+
+    p = plot()
+
+    # Plot density for the first filtered DataFrame (no fully adjusted)
+    if nrow(filtered_df_no_fa) > 0
+        density!(filtered_df_no_fa.score, label="No FA layer", color=:blue, alpha=0.5, bandwidth=λ)
+        y_no_fa = repeat([0], nrow(filtered_df_no_fa))
+        scatter!(filtered_df_no_fa.score, y_no_fa, label=nothing, mc=:blue, alpha=0.25)
+    end
+
+    # Plot density for the second filtered DataFrame (fully adjusted)
+    if nrow(filtered_df_fa) > 0
+        density!(filtered_df_fa.score, label="w/ FA layer", color=:red, alpha=0.5, bandwidth=λ)
+        y_fa = repeat([0], nrow(filtered_df_fa))
+        scatter!(filtered_df_fa.score, y_fa, label=nothing, mc=:red, alpha=0.25)
+    end
+
+    # Set plot limits and labels
+    xlims!(0, 1)
+    xlabel!("Score")
+    ylabel!("Density")
+    title!("Layer Depth: $tree_depth")
+    plot!(size=(400, 400))
+
+    return p
+end
+
+tree_depths = unique(data.tree_depth)
+
+plots = []
+for tree_depth in tree_depths
+    push!(plots, plot_score_distribution(data, tree_depth))
+end
+
+plot_grid = plot(plots..., layout=grid(length(tree_depths), 1), size=(500, 1000))
+
+savefig(plot_grid, "results/kde_plots_by_tree_depth.svg")
